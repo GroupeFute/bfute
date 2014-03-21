b15f01df73b42b6fe475099614507fc9fb38540b
JackPhillips <s.benrej@gmail.com>
2014-03-21 11:14:33 +0100

    MVVM vidé on purpose

--- bfute/src/fute/FichierMetier.java
+++ bfute/src/fute/FichierMetier.java
@@ -3,6 +3,9 @@ package fute;
 import java.util.ArrayList;
 
 public class FichierMetier {
+	
+	private boolean saved= false;
+	
 	private String nomDuMetier="";
 
 	private String coderome;
@@ -10,7 +13,8 @@ public class FichierMetier {
 	private String insee;
 	private String edDefinition = "<h2><b>1. INTITULES SYNONYMES OU APPARENTES</b></h2><h2><b>2. DEFINITION</b></h2><h2><b>3. FORMATION - QUALIFICATION</b></h2><h2 ><b>4. PRESENTATION DE L'ACTIVITE PRINCIPALE </b></h2><h3 style=\"margin-left: 20px;\"><b>4.1. LIEUX D'ACTIVITE</b></h3><ul  style=\"margin-left: 20px;\"><li>Type d'établissement : </li> <li>Environnement physique de travail :</li></ul><h3 style=\"margin-left: 20px;\"><b>4.2. DESCRIPTION DE L'ACTIVITE</b></h3><h3   style=\"margin-left: 20px;\"><b>4.3. MACHINES ET OUTILS UTILISES</b></h3><h3   style=\"margin-left: 20px;\"><b>4.4. PRODUITS ET MATERIAUX</b></h3><h3   style=\"margin-left: 20px;\"><b>4.5. PUBLIC ET RELATIONS SOCIALES</b></h3><h3   style=\"margin-left: 20px;\"><b>4.6. EXIGENCES PARTICULIERES</b></h3><h3   style=\"margin-left: 20px;\"><b>4.7. TRAVAILLEURS HANDICAPES</b></h3><h2><b>5. ACTIVITES POUVANT ETRE ASSOCIEES</b></h2><h2><b>6. DANGERS</b></h2><h3  style=\"margin-left: 20px;\"><b>6.1. ACCIDENTS DU TRAVAIL</b></br><h3  style=\"margin-left: 20px;\"><b>6.2. AMBIANCES ET CONTRAINTES PHYSIQUES</b></h3><h3  style=\"margin-left: 20px;\"><b>6.3. AGENTS CHIIMIQUES</b></h3><h3  style=\"margin-left: 20px;\"><b>6.4. AGENTS BIOLOGIQUES</b></h3><h3  style=\"margin-left: 20px;\"><b>6.5. CONTRAINTES ORGANISATIONNELLES ET RELATIONNELLES</b></h3><h2><b>7. RISQUES POUR LA SANTE</b></h2><h3  style=\"margin-left: 20px;\"><b>7.1. MALADIES PROFESSIONNELLES</b></h3><h3  style=\"margin-left: 20px;\"><b>7.2. AUTRES RISQUES</b></h3><h2><b>8. SURVEILLANCE MEDICALE</b></h2><h3  style=\"margin-left: 20px;\"><b>8.1. VISITE MEDICALE</b></h3><h3  style=\"margin-left: 20px;\"><b>8.2. EXAMENS COMPLEMENTAIRES</b></h3>  <h3  style=\"margin-left: 20px;\"><b>8.3. VACCINATIONS</b></h3><h3  style=\"margin-left: 20px;\"><b>8.4. SUIVI POST PROFESSIONNEL</b></h3><h3  style=\"margin-left: 20px;\"><b>8.5. DOSSIER MEDICAL</b></h3><h2><b>9. NUISANCES POUR L'ENVIRONNEMENT EXTERIEUR</b></h2><h2><b>10. ACTIONS PREVENTIVES</b></h2><h3  style=\"margin-left: 20px;\"><b>10.1. INDICATEURS D'AMBIANCE ET METROLOGIE</b></h3><h3  style=\"margin-left: 20px;\"><b>10.2. PREVENTION COLLECTIVE</b></h3><h3  style=\"margin-left: 20px;\"><b>10.3. PREVENTION INDIVIDUELLE</b></h3><h3  style=\"margin-left: 20px;\"><b>10.4. FORMATION - INFORMATION - SENSIBILISATION</b></h3><h2><b>11. REGLEMENTATION</b></h2><h3  style=\"margin-left: 20px;\"><b>11.1. TEXTES LEGISLATIFS ET REGLEMENTAIRES</b></h3><h3  style=\"margin-left: 20px;\"><b>11.2. RECOMMANDATIONS</b></h3><h3  style=\"margin-left: 20px;\"><b>11.3. NORMES</b></h3><h3  style=\"margin-left: 20px;\"><b>11.4. CONVENTIONS COLLECTIVES</b></h3><h2 ><b>12. BIBLIOGRAPHIE</b></h2><h2 ><b>13. ADRESSES UTILES</b></h2> ";
 	private ArrayList <String> niveaux = new ArrayList<String>();
-
+	
+	
 	private String commentaireNiveauF;
 
 
@@ -28,6 +32,19 @@ public class FichierMetier {
 	private ArrayList<String> thesoVisu1 = new ArrayList<String>();
 
 
+	
+	
+	
+	public boolean isSaved() {
+		return saved;
+	}
+
+
+	public void setSaved(boolean saved) {
+		this.saved = saved;
+	}
+
+
 	public int getNumeroFiche() {
 		return numeroFiche;
 	}
@@ -39,7 +56,7 @@ public class FichierMetier {
 
 
 	public FichierMetier(){
-
+		
 	}
 
 
